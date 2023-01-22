73f006730ab54e860496d51a6f17f482f774193d
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-22 14:10:20 +0100

    Implementato modal di Bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,16 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+const modalDeleteBtn = document.querySelectorAll('.modal-delete-btn');
+const modalForm = document.querySelector('.modal-form');
+const whichCar = document.getElementById('which-one');
+
+modalDeleteBtn.forEach(elm => { 
+    elm.addEventListener('click', function(){
+        modalForm.action = "";
+        const id = elm.getAttribute('data-id');
+        modalForm.action += `/${id}`;
+        whichCar.innerHTML = `record with id number ${id}.`;
+    })
+});
--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -41,16 +41,38 @@
                             <a href="{{ route('cars.edit', $car->id) }}">
                                 <button class="btn btn-light">Edit</button>
                             </a>
-                            <form action="{{ route('cars.destroy', $car->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger modal-delete-btn" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="{{$car->id}}">
+                                Delete
+                            </button>
                         </td>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    @endforeach
+                </tbody>
+            </table>
     </section>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5 text-dark" id="exampleModalLabel">Deleting <span id="which-one">{{ null }}</span></h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-dark">
+                    Are you sure?
+                </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <form class="modal-form" action="{{ null }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+
